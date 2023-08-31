0f4a8371493af9bf926c4037174466291a8cd7d0
Saurabh Kaplas <107455544+skpkss@users.noreply.github.com>
2023-09-01 01:59:33 +0530

    react

---
+++ ConnectTheWallet.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function ConnectTheWallet() {
+    return (
+        <div className='text-center h-full'>
+            <h1 className='text-2xl m-20'>
+                Please Connect with your Web3 Wallet
+            </h1>
+        </div>
+    )
+}
+
+export default ConnectTheWallet
